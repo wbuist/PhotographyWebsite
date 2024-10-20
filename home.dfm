fc77ec83fae98b9b6415a20c90d7a05bf6bd0d76
William Buist <william@williambuist.com>
2024-10-20 20:39:50 +0100

    Lots of updates as templates developed

---
+++ home.dfm
@@ -0,0 +1,13 @@
+object Form1: TForm1
+  Width = 1280
+  Height = 960
+  CSSLibrary = cssBootstrap
+  ElementFont = efCSS
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -30
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  ParentFont = False
+  PixelsPerInch = 192
+end
---
+++ photowebsite.dpr
@@ -0,0 +1,19 @@
+program photowebsite;
+
+
+
+{$R *.dres}
+
+uses
+  Vcl.Forms,
+  WEBLib.Forms,
+  home in 'home.pas' {Form1: TWebForm} {*.html};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
